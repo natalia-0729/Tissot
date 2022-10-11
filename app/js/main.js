78c0a6b5828f15c93afe1dfe5734650b44f13cc3
natalia-0729 <you@example.com>
2022-10-11 21:34:44 +0300

    completed the layout

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
 $(function(){
-    
+    $(".menu a, .logo, .header__btn").on("click", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
 });
